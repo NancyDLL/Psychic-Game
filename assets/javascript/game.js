772002d2baa13f84bab949ce7427f0e1f34f3e8a
NancyDLL <nano14@mac.com>
2017-07-01 10:37:58 -0500

    Started Javascript file and psuedocoded the script.

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+//Check connection to html page
+console.log("connected")
+
+//1 - Computer makes a choice - choice is stored in a variable.
+
+//2 - User makes a choice - choice is stored in a variable.
+
+//3 - Computer choice is compared to User choice - determine who gets the win.
+
+//4 - Script tracks wins and losses.
+
+//5 - Script delivers to html the number of wins and losses.
+
+//6 - Script tracks number of  User choices and counts against the limit.
+
+//7 - Script delivers to html the number of guesses left and the number guessed so far.
+
+//8 - When limit is reached game the script delivers end of game message and stops running.
