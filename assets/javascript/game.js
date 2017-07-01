216768ba4607594035063b880e1039117fa2409c
NancyDLL <nano14@mac.com>
2017-07-01 11:11:16 -0500

    Guesses completed for script.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,17 +2,24 @@
 console.log("connected")
 
 //1 - Computer makes a choice - choice is stored in a variable.
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerGuess = options[Math.floor(Math.random()*options.length)];
+console.log("The Computer guessed " + computerGuess);
 
 //2 - User makes a choice - choice is stored in a variable.
+document.onkeyup = function(){
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log("The User guessed " + userGuess);
+}
 
 //3 - Computer choice is compared to User choice - determine who gets the win.
 
 //4 - Script tracks wins and losses.
 
-//5 - Script delivers to html the number of wins and losses.
+//5 - Display result of wins and losses to user.
 
 //6 - Script tracks number of  User choices and counts against the limit.
 
-//7 - Script delivers to html the number of guesses left and the number guessed so far.
+//7 - Display result of the number of guesses left and the number guessed so far to User.
 
 //8 - When limit is reached game the script delivers end of game message and stops running.
