84cd226f17d0ed7099a6cb5744e07458f9498b9e
NancyDLL <nano14@mac.com>
2017-07-02 11:42:52 -0500

    Got basic comparison function working.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,45 @@
 //Check connection to html page
 console.log("connected")
 
-//1 - Computer makes a choice - choice is stored in a variable.
+//Set the initial global variables
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var computerGuess = options[Math.floor(Math.random()*options.length)];
-console.log("The Computer guessed " + computerGuess);
+var userGuess = null;
+var guessesLeft = 9;
+var guessesMade = [];
+var wins = 0;
+var losses = 0;
 
-//2 - User makes a choice - choice is stored in a variable.
+
+
+//1 - create a function
 document.onkeyup = function(){
+	//2 - Computer makes a choice - choice is stored in a variable.
+	var computerGuess = options[Math.floor(Math.random()*options.length)];
+	console.log("The Computer guessed " + computerGuess);
+
+	//3 - User makes a choice - choice is stored in a variable.
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 	console.log("The User guessed " + userGuess);
+
+	//4 - Computer choice is compared to User choice - determine who gets the win.
+	if (userGuess == computerGuess) {
+ 	console.log("same");
+	} else {
+ 	console.log("different");
+ }
 }
 
 
 
 
+//4 - Script tracks number of User choices and counts against the limit.
 
-//3 - Computer choice is compared to User choice - determine who gets the win.
-// if (computerGuess == userGuess) {
-// 	console.log("same");
-// } else {
-// 	console.log("different");
-// }
+//5 - Display result of the number of guesses left and the number guessed so far to User.
 
-//4 - Script tracks wins and losses.
+//6 - Script tracks wins and losses.
 
-//5 - Display result of wins and losses to user.
+//7 - Display wins, losses, and guesses to User
 
-//6 - Script tracks number of  User choices and counts against the limit.
 
-//7 - Display result of the number of guesses left and the number guessed so far to User.
 
-//8 - When limit is reached game the script delivers end of game message and stops running.
+
