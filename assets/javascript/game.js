204d824484be6d8136cf3d13aac1f96a894ad842
NancyDLL <nano14@mac.com>
2017-07-02 12:47:11 -0500

    Got more of the function built and display to the html page.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
-//Check connection to html page
+//1 - Check connection to html page
 console.log("connected")
 
-//Set the initial global variables
+//2 - Set the initial global variables
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var userGuess = null;
 var guessesLeft = 9;
@@ -10,23 +10,37 @@ var wins = 0;
 var losses = 0;
 
 
-
-//1 - create a function
+//3 - create a function
 document.onkeyup = function(){
-	//2 - Computer makes a choice - choice is stored in a variable.
+	//3.1 - Computer makes a choice - choice is stored in a variable.
 	var computerGuess = options[Math.floor(Math.random()*options.length)];
 	console.log("The Computer guessed " + computerGuess);
 
-	//3 - User makes a choice - choice is stored in a variable.
+	//3.2 - User makes a choice - choice is stored in a variable.
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 	console.log("The User guessed " + userGuess);
 
-	//4 - Computer choice is compared to User choice - determine who gets the win.
+	//3.3 - Computer choice is compared to User choice.
 	if (userGuess == computerGuess) {
  	console.log("same");
+ 	wins++;
+ 	console.log("The user has won " + wins + " times.");
+
 	} else {
  	console.log("different");
- }
+ 	guessesMade++;
+ 	console.log("The user has guessed " + guessesMade + " times.");
+ 		if (guessesMade == guessesLeft) {
+ 			losses++;
+ 		}
+ 	}
+
+ 	//3.4 - Display user Guess, wins, losses, guesses left
+ 	document.querySelector('#winsCount').innerHTML = wins;
+ 	document.querySelector('#lossesCount').innerHTML = losses;
+ 	document.querySelector('#guessesLeft').innerHTML = guessesLeft;
+ 	document.querySelector('#guessesSoFar').innerHTML = userGuess;
+
 }
 
 
