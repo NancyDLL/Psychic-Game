eb82d393ffe72fbf99ac3b966477a3a2029064af
NancyDLL <nano14@mac.com>
2017-07-02 13:41:55 -0500

    Display of letters working, script tested, and complete.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,51 +13,42 @@ var losses = 0;
 //3 - create a function
 document.onkeyup = function(){
 
+	//3.1 - Computer makes a choice - choice is stored in a variable.
+	var computerGuess = options[Math.floor(Math.random()*options.length)];
+	console.log("The Computer guessed " + computerGuess);
+
+	//3.2 - User makes a choice - choice is stored in a variable.
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log("The User guessed " + userGuess);
+
+	//3.3 - Computer choice is compared to User choice.
+	if (userGuess == computerGuess) {
+	 	console.log("same");
+	 	//count win
+	 	wins++;
+	 	console.log("The user has won " + wins + " times.");
+	 	//reset guessesLeft count to 9
+	 	guessesLeft=9;
 		
-		//3.1 - Computer makes a choice - choice is stored in a variable.
-		var computerGuess = options[Math.floor(Math.random()*options.length)];
-		console.log("The Computer guessed " + computerGuess);
-
-		//3.2 - User makes a choice - choice is stored in a variable.
-		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-		console.log("The User guessed " + userGuess);
-
-		//3.3 - Computer choice is compared to User choice.
-		if (userGuess == computerGuess) {
-		 	console.log("same");
-		 	wins++;
-		 	//reset guessesLeft count to 9
-		 	guessesLeft=9;
-			console.log("The user has won " + wins + " times.");
-		} else {
-		 	console.log("different");
-		 	guessesMade++;
-		 	guessesLeft--;
-		 	console.log("The user has guessed a total of " + guessesMade + " times.");
-		 		if (guessesLeft == 0) {
-		 			losses++;
-		 			guessesLeft=9;
-	 			}
-	 		}
+	} else {
+	 	console.log("different");
+	 	guessesMade++;
+	 	guessesLeft--;
+	 	document.querySelector('#guessesSoFar').innerHTML += userGuess + " ";
+	 	console.log("The user has guessed a total of " + guessesMade + " times.");
+	 		if (guessesLeft == 0) {
+	 			losses++;
+	 			guessesLeft=9;
+	 			userGuess = null;
+	 			document.querySelector('#guessesSoFar').innerHTML = userGuess;
+ 			}
+ 		}
 
  	//3.4 - Display user Guess, wins, losses, guesses left
  	document.querySelector('#winsCount').innerHTML = wins;
  	document.querySelector('#lossesCount').innerHTML = losses;
  	document.querySelector('#guessesLeft').innerHTML = guessesLeft;
- 	document.querySelector('#guessesSoFar').innerHTML = userGuess;
-
 }
-
-
-
-
-//4 - Script tracks number of User choices and counts against the limit.
-
-//5 - Display result of the number of guesses left and the number guessed so far to User.
-
-//6 - Script tracks wins and losses.
-
-//7 - Display wins, losses, and guesses to User
 
 
 
